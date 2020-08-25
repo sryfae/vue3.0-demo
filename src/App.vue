d0d05067bd97eee81d28b7e5ac8520b72bec2bec
sunrongyuan <sry@sryria.lan>
2020-08-25 23:37:10 +0800

    firstpush

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    {{a}}
+    <button @click="add">add</button>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import {reactive, ref, effect} from '@vue/reactivity';
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // },
+  setup() {
+    let a = ref(0);
+    const obj = reactive({ count: 0 })
+    console.log(obj);
+    let dummy = a.value;
+    effect(() => {
+      dummy = a.value;
+      console.log(dummy);
+    });
+    function add() {
+      a.value++;
+    }
+    return {
+      a,
+      add
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
